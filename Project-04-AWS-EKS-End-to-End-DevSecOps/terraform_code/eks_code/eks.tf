86b1f6a9c43c8439d7b79c611d0878f8c4338797
arulkumar27 <arulvcm27@gmail.com>
2026-09-12 11:30:00 +0530

    Update EKS node capacity and AWS provider

--- Project-04-AWS-EKS-End-to-End-DevSecOps/terraform_code/eks_code/eks.tf
+++ Project-04-AWS-EKS-End-to-End-DevSecOps/terraform_code/eks_code/eks.tf
@@ -22,9 +22,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     panda-node = {
-      min_size     = 1
+      min_size     = 2
       max_size     = 2
-      desired_size = 1
+      desired_size = 2
 
       instance_types = ["t3.small"]
       capacity_type  = "SPOT"
