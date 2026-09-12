fb7897f621dad2593cbf11ae2cf1f5e034c1d2e7
arulkumar27 <arulvcm27@gmail.com>
2026-09-12 10:33:17 +0530

    Update EKS Terraform configuration

--- Project-04-AWS-EKS-End-to-End-DevSecOps/terraform_code/eks_code/eks.tf
+++ Project-04-AWS-EKS-End-to-End-DevSecOps/terraform_code/eks_code/eks.tf
@@ -22,15 +22,15 @@ module "eks" {
 
   eks_managed_node_groups = {
     panda-node = {
-      min_size     = 2
-      max_size     = 4
-      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t3.small"]
       capacity_type  = "SPOT"
 
       tags = {
-        ExtraTag = "Panda_Node"
+        ExtraTag = "amazon-prime-node"
       }
     }
   }
